ffc51a4682c6156124dfc2309783779c8c41ae37
Mohit Bagdi <92544103+edito0@users.noreply.github.com>
2023-07-06 10:27:38 +0530

    Update dev.js

--- config/dev.js
+++ config/dev.js
@@ -1,6 +1,1 @@
-module.exports={
-    MONGOURI:"mongodb+srv://mohitbagdi280:KpYerhjyWVLaxC2P@cluster0.w3xl9ys.mongodb.net/?retryWrites=true&w=majority",
-    JWT_SECRET:"asfdsifkjbcfbiadsaobsdvbdbsd",
-    Email : "sparofficialmail@gmail.com",
-    Password : "yniypnlsmtpxdrdn"
-} 
+
